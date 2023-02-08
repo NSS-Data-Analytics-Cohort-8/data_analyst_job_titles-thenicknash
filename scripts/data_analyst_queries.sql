0346865d8213e7bc0ce300714b35f79e6001aaee
Nick Nash <nicholas.nash@cashtn.com>
2023-02-07 19:27:56 -0600

    completed questions 10-12

--- scripts/data_analyst_queries.sql
+++ scripts/data_analyst_queries.sql
@@ -86,9 +86,32 @@ group by company;
 
 -- 10. Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 
+select company, avg(star_rating) as avg_rating
+from data_analyst_jobs
+where review_count > 5000
+group by company
+order by avg_rating desc;
+
+-- General Motors, Unilever, Microsoft, Nike, American Express, and Kaiser Permanente all have ~ 4.19 average star ratings.
+
 -- 11. Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
 
+select distinct title
+from data_analyst_jobs
+where lower(title) like '%analyst%';
+
+-- There are 323 different job titles containing the word 'Analyst'.
+
 -- 12. How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+
+select distinct title
+from data_analyst_jobs
+where (
+	lower(title) not like '%analyst%'
+	or lower(title) not like '%analytics%'
+);
+
+-- There are 801 different job titles that do not contain the word ‘Analyst’ or the word ‘Analytics’.
 
 -- BONUS: You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks.
 
